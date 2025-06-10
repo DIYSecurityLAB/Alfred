b7a87087d330196b4f65b59c7528a85d3a51bdaa
lucasqc04 <lucasqcampos9@gmail.com>
2025-06-10 12:21:40 -0300

    feat(tour): remove initial tour

--- src/view/screens/App.tsx
+++ src/view/screens/App.tsx
@@ -1,46 +1,16 @@
 import { AuthProvider } from '@/view/context/AuthContext';
-import { TourProvider } from '@reactour/tour';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/react';
-import { useTranslation } from 'react-i18next';
 import { BrowserRouter } from '../routes/BrowserRouter';
 
-const isFirstVisit = !localStorage.getItem('tourCompleted');
-
 export function App() {
-  const { t } = useTranslation();
-
-  const steps = [
-    {
-      selector: '.image-step',
-      content: t('guide.imageStep'),
-      highlightedSelectors: ['.ReactModal__Content'],
-      mutationObservables: ['.ReactModal__Overlay'],
-    },
-    {
-      selector: '.brl-step',
-      content: t('guide.brlStep'),
-    },
-    {
-      selector: '.proceed-button-step',
-      content: t('guide.proceedButtonStep'),
-    },
-  ];
-
   return (
     <>
       <Analytics />
       <SpeedInsights />
-      <TourProvider
-        steps={steps}
-        disableInteraction={isFirstVisit}
-        defaultOpen={isFirstVisit}
-        beforeClose={() => localStorage.setItem('tourCompleted', 'true')}
-      >
-        <AuthProvider>
-          <BrowserRouter />
-        </AuthProvider>
-      </TourProvider>
+      <AuthProvider>
+        <BrowserRouter />
+      </AuthProvider>
     </>
   );
 }
