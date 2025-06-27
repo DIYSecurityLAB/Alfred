53e25c258410ed63ba2538339514bcba2f4dc5aa
Portela03 <joao.portela03@gmail.com>
2025-06-27 04:52:17 +0000

    fix(coupons): corrigir indentação na mensagem de erro para uso excessivo do cupom "ZERO"

--- src/view/screens/Checkout/DataForm/useDataForm.ts
+++ src/view/screens/Checkout/DataForm/useDataForm.ts
@@ -786,7 +786,7 @@ Estou comprando mais de 5 mil reais no Alfred e preciso do formulário de Valida
         setCupom('');
         toast.error(
           error.response?.data?.message ||
-          t('Você usou o cupom "ZERO" mais de 1 vez.'),
+            t('Você usou o cupom "ZERO" mais de 1 vez.'),
         );
         return;
       }
